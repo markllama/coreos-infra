2ee5d43401e2c64b5bdec7291555064200fa8a9c
Mark Lamourine <markllama@gmail.com>
2025-01-24 09:49:12 -0500

    checkpoint before refactoring coreos deploy

--- scripts/prepare-pi.sh
+++ scripts/prepare-pi.sh
@@ -24,8 +24,11 @@ FIRMWARE_URL=https://github.com/pftf/RPi4/releases/download/v${FIRMWARE_VERSION}
 FIRMWARE_ZIP=RPi4_UEFI_Firmware_v${FIRMWARE_VERSION}.zip
 
 # Install CoreOS onto the SD card
-sudo coreos-installer install --stream stable --architecture aarch64 --platform rpi4 \
+sudo coreos-installer install --stream stable --architecture aarch64 \
      --ignition-file ${IGNITION_FILE} ${FCOS_DISK}
+
+sync
+sleep 2
 
 # Determine which partition will contain the EFI firmware files
 EFIPART=$(lsblk $FCOS_DISK -J -oLABEL,PATH  |
--- scripts/prepare-sdcard.sh
+++ scripts/prepare-sdcard.sh
@@ -9,7 +9,7 @@ FIRMWARE_URL=https://github.com/pftf/RPi4/releases/download/v${FIRMWARE_VERSION}
 FIRMWARE_ZIP=RPi4_UEFI_Firmware_v${FIRMWARE_VERSION}.zip
 
 # Install CoreOS onto the SD card
-sudo coreos-installer install --stream stable --architecture aarch64 --platform rpi4 \
+sudo coreos-installer install --stream stable --architecture aarch64 --platform metal \
      --ignition-file ${IGNITION_FILE} ${FCOS_DISK}
 
 # Determine which partition will contain the EFI firmware files
